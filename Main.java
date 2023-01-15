bb38039a6bc62a354ab1d4498ae336e40b513a48
Silvan <silvan.pawlowski@yahoo.de>
2023-01-15 19:31:37 +0100

    k

--- Main.java
+++ Main.java
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import Pong.PongWindow; 
-=======
-import Pong.Pong;
 import SpaceInvaders.SpaceInvader; 
->>>>>>> 086c381ce23a896372dff66839a233ad02d2c340
 
 public class Main {
     public static void main(String[] args) {
