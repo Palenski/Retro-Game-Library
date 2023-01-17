042eb4201390ed651b7988ce741a2c1b0ef29c85
Theo-Felix <theo@theo-felix.de>
2023-01-17 14:03:16 +0100

    a

--- SpaceInvaders/SpaceInvader.java
+++ SpaceInvaders/SpaceInvader.java
@@ -1,5 +1,6 @@
 package SpaceInvaders;
 
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,9 +16,10 @@ public SpaceInvader(){
 
         frame.setSize(1000, 650);
 
-        frame.setTitle("Anmeldung");
+        frame.setTitle("Space Invaders");
         frame.setLayout(null);
         frame.setVisible(true);
         frame.setResizable(false);
+        frame.getContentPane().setBackground(Color.black);
 }
 }
