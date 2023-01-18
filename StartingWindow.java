fa574ab0292d3f023f231933d585bd435622c65d
Theo-Felix <theo@theo-felix.de>
2023-01-18 10:34:55 +0100

    verknüpfungen von startingwindow zu games

--- StartingWindow.java
+++ StartingWindow.java
@@ -2,20 +2,31 @@
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+
+import Pong.PongWindow;
+import TypingGame.TypingGame;
 
 public class StartingWindow implements ActionListener {
 
     JFrame frame = new JFrame();
 
-    JButton typingButton = new JButton();
+    JButton typingButton = new JButton("Typing Game");
+    JButton pongButton = new JButton("Pong");
 
     StartingWindow() {
 
         typingButton.addActionListener(this);
+        typingButton.setBounds(255, 50, 500, 55);
         frame.add(typingButton);
 
+
+        pongButton.addActionListener(this);
+        pongButton.setBounds(255, 125, 500, 55);
+        frame.add(pongButton);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1750, 750);
@@ -32,10 +43,18 @@ public class StartingWindow implements ActionListener {
     public void actionPerformed(ActionEvent e) { // einfache Weiterleitung an die verschiedenen Fenster
         if (e.getSource() == typingButton) {
             frame.dispose();
+            TypingGame typingGame = new TypingGame();
             
         }
 
+        if (e.getSource() == pongButton) {
+            frame.dispose();
+            PongWindow pongWindow = new PongWindow();
+            
         }
+        }
+
+    
     }
 
 
