ea3b2fcfea9c5fca8df6c171576adfcdedd08b4c
Silvan <silvan.pawlowski@yahoo.de>
2023-03-07 12:47:57 +0100

    d

--- StartingWindow2.java
+++ StartingWindow2.java
@@ -1,12 +1,17 @@
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Image;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.sql.SQLException;
 
+import javax.imageio.ImageIO;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -16,50 +21,74 @@ public class StartingWindow2 implements ActionListener {
 
     JFrame frame = new JFrame();
 
-    JButton titleButton = new JButton("Wähle ein Spiel");
 
-    JButton pongButton = new JButton("Pong");
-    JButton tictactoeButton = new JButton("TicTacToe");
+   
+
+
+
+    JButton titleButton = new JButton("Wähle ein Spiel");
+    
+
+    Icon normal = new ImageIcon(getClass().getResource("Images/Pong.png"));
+  
+    
+    JButton pongButton = new JButton(normal);
+
+    JButton tictactoeButton = new JButton();
+    //static ImageIcon iconTicTactToe = new ImageIcon();
 
     JButton typingGameButton = new JButton("Typing Game");
+    static ImageIcon icontypigingGame = new ImageIcon();
+
     JButton spaceInvadersButton = new JButton("Space Invader");
+    static ImageIcon iconSpaceInvaders = new ImageIcon();
 
     JButton snakeButton = new JButton("Snake");
+    static ImageIcon iconSnakeButton = new ImageIcon();
+
+    JButton spiel6 = new JButton("6");
+    static ImageIcon iconSpiel6 = new ImageIcon();
 
     JButton logOutButton = new JButton("Beenden");
 
-    public static int Test = 0;
 
     StartingWindow2() {
 
-        pongButton.setBounds(5, 110, 398, 50);
+        pongButton.setBounds(8, 110, 398, 125);
         pongButton.setFocusable(false);
         pongButton.addActionListener(this);
 
-        tictactoeButton.setBounds(5, 170, 398, 50);
+        tictactoeButton.setBounds(8, 245, 398, 125);
         tictactoeButton.setFocusable(false);
         tictactoeButton.addActionListener(this);
+      
+        tictactoeButton.setIcon(new ImageIcon("Pong.png"));
 
-        typingGameButton.setBounds(5, 230, 398, 50);
+
+        typingGameButton.setBounds(8, 380, 398, 125);
         typingGameButton.setFocusable(false);
         typingGameButton.addActionListener(this);
 
-        spaceInvadersButton.setBounds(5, 290, 398, 50);
+        spaceInvadersButton.setBounds(415, 110, 398, 125);
         spaceInvadersButton.setFocusable(false);
         spaceInvadersButton.addActionListener(this);
 
-        snakeButton.setBounds(5,350,398,50);
+        snakeButton.setBounds(415,245,398,125);
         snakeButton.setFocusable(false);
         snakeButton.addActionListener(this);
 
-        logOutButton.setBounds(5, 350, 398, 50);
+        spiel6.setBounds(415, 380, 398, 125);
+        spiel6.setFocusable(false);
+        spiel6.addActionListener(this);
+
+        logOutButton.setBounds(8, 515, 805, 125);
         logOutButton.setFocusable(false);
         logOutButton.addActionListener(this);
 
-        titleButton.setBounds(0, 0, 410, 100);
+        titleButton.setBounds(0, 0, 820, 100);
         titleButton.addActionListener(this);
         titleButton.setFocusable(false);
-        titleButton.setFont(new Font(null, Font.BOLD, 23));
+        titleButton.setFont(new Font(null, Font.BOLD, 43));
         titleButton.setContentAreaFilled(false);
         titleButton.setBorderPainted(false);
 
@@ -70,14 +99,19 @@ public class StartingWindow2 implements ActionListener {
         frame.add(typingGameButton);
         frame.add(spaceInvadersButton);
         frame.add(snakeButton);
+        frame.add(spiel6);
         frame.add(logOutButton);
 
+        
+        
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(420, 450);
+        frame.setSize(837, 750);
         frame.setTitle("Startscreen");
         frame.setLayout(null);
         frame.setVisible(true);
         frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.getContentPane().setBackground(Color.PINK);
     }
 
     public void actionPerformed(ActionEvent e) { // einfache Weiterleitung an die verschiedenen Fenster
