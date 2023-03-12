e1b7bb9311d509d1a3bc5a216a3edd25f876f57c
Theo-Felix <theo@theo-felix.de>
2023-03-12 14:01:54 +0100

    a

--- SpaceInvaders/SpaceInvader.java
+++ SpaceInvaders/SpaceInvader.java
@@ -6,20 +6,25 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class SpaceInvader {
     JFrame frame = new JFrame();
+    JPanel Window = new Window();
+
+    public static Window window;
 
 public SpaceInvader(){
 
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        frame.setSize(1000, 650);
+    frame.setSize(600, 600);
 
-        frame.setTitle("Space Invaders");
-        frame.setLayout(null);
-        frame.setVisible(true);
-        frame.setResizable(false);
-        frame.getContentPane().setBackground(Color.black);
+    frame.setTitle("Space Invaders");
+    frame.setLayout(null);
+    frame.setVisible(true);
+    frame.setResizable(false);
+    frame.setLocationRelativeTo(null);
+    frame.setContentPane(Window);
 }
 }
