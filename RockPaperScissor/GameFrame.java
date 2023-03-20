2c897b8d551e7c7cf44cd7c022e3080118b4a879
Theo-Felix <theo@theo-felix.de>
2023-03-20 14:49:06 +0100

    Schere Stein Papier fertig nur hässlich

--- RockPaperScissor/GameFrame.java
+++ RockPaperScissor/GameFrame.java
@@ -13,14 +13,14 @@ public class GameFrame extends JFrame implements ActionListener {
     JPanel buttonPanel = new JPanel();
     JLabel[] labels = new JLabel[2];
     JPanel labelPanel = new JPanel();
-    JLabel label = new JLabel();
-    JLabel PCLabel = new JLabel();
+    JLabel Ergebnis = new JLabel("Das Ergebnis ist: ");
+    JPanel Ergebniss = new JPanel();
     String[] stp = { "Schere", "Stein", "Papier" };
-    String[] Texte = {"Hier steht was du wählst", "Hier steht was der Computer wählt"};
-    String Text1 = "Hier steht was du wählst";
+    String[] Texte = { "Hier steht was du wählst", "Hier steht was der Computer wählt" };
     String Text2 = "Deine Wahl ist: ";
-    String Text3 = "Hier steht was der Computer wählt       ";
     String Text4 = "Der Computer wählt: ";
+    String PlayerChoice;
+    String ComputerChoice;
 
     public static void main(String[] args) {
         GameFrame gameFrame = new GameFrame();
@@ -30,16 +30,13 @@ public class GameFrame extends JFrame implements ActionListener {
 
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //frame.setLocationRelativeTo(null);
+        // frame.setLocationRelativeTo(null);
         frame.setSize(800, 700);
         frame.setTitle("Schere Stein Papier");
 
         buttonPanel.setLayout(new GridLayout(1, 0));
         buttonPanel.setBackground(new Color(150, 150, 150));
         buttonPanel.setSize(300, 100);
-
-        
-        
 
         for (int i = 0; i < 3; i++) {
             buttons[i] = new JButton();
@@ -54,45 +51,95 @@ public class GameFrame extends JFrame implements ActionListener {
 
         }
 
-        label.setText(Text1);
-        PCLabel.setText(Text3);
+        labelPanel.setLayout(new GridLayout(1, 0));
 
-        labelPanel.setLayout(new GridLayout(1,0));
+        for (int i = 0; i < 2; i++) {
 
-        for(int i = 0; i < 2; i++){
-            
-                labels[i] = new JLabel();
-                labelPanel.add(labels[i]);
-                labels[i].setText(Texte[i]);
+            labels[i] = new JLabel();
+            labelPanel.add(labels[i]);
+            labels[i].setText(Texte[i]);
         }
+
+        Ergebniss.add(Ergebnis);
 
         frame.add(buttonPanel, BorderLayout.SOUTH);
-        frame.add(labelPanel,BorderLayout.NORTH);
+        frame.add(labelPanel, BorderLayout.NORTH);
+        frame.add(Ergebniss, BorderLayout.CENTER);
     }
 
-    
-    public void PCTurn(){
+    public void PCTurn() {
         int PCWahl = random.nextInt(3);
-        if(PCWahl == 0){
+        if (PCWahl == 0) {
             labels[1].setText(Text4 + stp[0]);
+            ComputerChoice = stp[0];
         }
-        if(PCWahl == 1){
+        if (PCWahl == 1) {
             labels[1].setText(Text4 + stp[1]);
+            ComputerChoice = stp[1];
         }
-        if(PCWahl == 2){
+        if (PCWahl == 2) {
             labels[1].setText(Text4 + stp[2]);
+            ComputerChoice = stp[2];
         }
-        
+
+        GewinnerErmitlung();
     }
 
-    @Override
     public void actionPerformed(ActionEvent e) {
-        for (int i = 0; i < 3; i++) {
-            if (e.getSource() == buttons[i]) {
-                labels[0].setText(Text2 + stp[i]);
+
+        if (e.getSource() == buttons[0]) {
+            labels[0].setText(Text2 + stp[0]);
+            PlayerChoice = buttons[0].getText();
+            for (int i = 0; i < 3; i++) {
+                buttons[i].setEnabled(false);
             }
-            buttons[i].setEnabled(false);
-            PCTurn();
+        }
+        if (e.getSource() == buttons[1]) {
+            labels[0].setText(Text2 + stp[1]);
+            PlayerChoice = buttons[1].getText();
+            for (int i = 0; i < 3; i++) {
+                buttons[i].setEnabled(false);
+            }
+        }
+        if (e.getSource() == buttons[2]) {
+            labels[0].setText(Text2 + stp[2]);
+            PlayerChoice = buttons[2].getText();
+            for (int i = 0; i < 3; i++) {
+                buttons[i].setEnabled(false);
+            }
+        }
+
+        PCTurn();
+
+    }
+
+    public void GewinnerErmitlung() {
+        if (PlayerChoice.equals("Schere") && ComputerChoice.equals("Schere")) {
+            Ergebnis.setText("Unentschieden");
+        }
+        if (PlayerChoice.equals("Schere") && ComputerChoice.equals("Papier")){
+            Ergebnis.setText("Du gewinnst");
+        }
+        if (PlayerChoice.equals("Schere") && ComputerChoice.equals("Stein")){
+            Ergebnis.setText("Der Computer gewinnt");
+        }
+        if (PlayerChoice.equals("Stein") && ComputerChoice.equals("Schere")){
+            Ergebnis.setText("Du gewinnst");
+        }
+        if (PlayerChoice.equals("Stein") && ComputerChoice.equals("Papier")){
+            Ergebnis.setText("Der Computer gewinnt");
+        }
+        if (PlayerChoice.equals("Stein") && ComputerChoice.equals("Stein")){
+            Ergebnis.setText("Unentschieden");
+        }
+        if (PlayerChoice.equals("Papier") && ComputerChoice.equals("Schere")){
+            Ergebnis.setText("Der Computer gewinnt");
+        }
+        if (PlayerChoice.equals("Papier") && ComputerChoice.equals("Papier")){
+            Ergebnis.setText("Unentschieden");
+        }
+        if (PlayerChoice.equals("Papier") && ComputerChoice.equals("Stein")){
+            Ergebnis.setText("Du gewinnst");
         }
     }
 }
